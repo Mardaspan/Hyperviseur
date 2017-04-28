8b22fc3be6fa3164b082b74c3aba53c9c3d85f0e
Nowadays <herespieceofmine@gmail.com>
2017-04-28 11:49:39 +0200

    Ajotu du projet initial

---
+++ ProjetIS/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Dim test = 0
+    Dim tableau As New ArrayList
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+
+End Class
